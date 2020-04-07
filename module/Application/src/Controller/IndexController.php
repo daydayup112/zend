9153eccc233a3548376a2cb1bb8f2333c9e040a1
Shi <shuo.shi@accenture.com>
2020-04-07 16:20:08 +0900

    shuo commit

---
+++ module/Application/src/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+    	error_reporting(E_ALL^E_WARNING^E_NOTICE);
+    	echo 3;
+        return new ViewModel();
+    }
+}
